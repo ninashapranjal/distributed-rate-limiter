a1a0ab20d4c50d95c8e2b4f1bda0dbcfe6c6526d
ninashapranjal <shahipranjal7@gmail.com>
2026-08-07 00:33:57 -0400

    execute token bucket algorithm

--- main.cpp
+++ main.cpp
@@ -8,32 +8,72 @@
 #include <chrono>
 
 using namespace sw::redis;
+using namespace std;
 
-std::string loadScript(const std::string& filename) {
-    std::ifstream file(filename);
+string loadScript(const string& filename) {
+    ifstream file(filename);
 
     if (!file.is_open()) {
-        throw std::runtime_error("Could not open Lua script");
+        throw runtime_error("Could not open Lua script");
     }
 
-    return std::string(
-        (std::istreambuf_iterator<char>(file)),
-        std::istreambuf_iterator<char>()
+    return string(
+        (istreambuf_iterator<char>(file)),
+        istreambuf_iterator<char>()
     );
 }
 
 int main() {
     try {
         Redis redis("tcp://127.0.0.1:6379");
-        std::cout << "Connected to Redis!\n";
+        cout << "Connected to Redis\n";
+
+        string script = loadScript("token_bucket.lua");
+
+        auto script_sha = redis.script_load(script);
+
+        cout << "Loaded Lua script:\n";
+        cout << script_sha << "\n\n";
+
+        std::string key = "ratelimit:user1";
+
+        int capacity = 10;
+        double refill_rate = 1;
+        int requested = 1;
+
+        auto now = std::chrono::duration<double>(
+            std::chrono::system_clock::now().time_since_epoch()
+        ).count();
+
+        std::vector<std::string> keys = {
+            key
+        };
+
+        std::vector<std::string> args = {
+        std::to_string(capacity),
+        std::to_string(refill_rate),
+        std::to_string(now),
+        std::to_string(requested)
+    };
+
+auto result = redis.evalsha<std::vector<long long>>(
+    script_sha,
+    keys.begin(),
+    keys.end(),
+    args.begin(),
+    args.end()
+);
+
+int allowed = static_cast<int>(result[0]);
+double remaining = result[1];
             
     } catch (const Error &err) {
-        std::cerr << "Redis error: "
+        cerr << "Redis error: "
                   << err.what()
                   << "\n";
 
-    } catch (const std::exception &err) {
-        std::cerr << "Error: "
+    } catch (const exception &err) {
+        cerr << "Error: "
                   << err.what()
                   << "\n";
     }
