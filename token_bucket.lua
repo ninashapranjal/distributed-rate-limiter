770e18c8deaca591f100665160a8fbdd8e5d026a
ninashapranjal <shahipranjal7@gmail.com>
2026-08-14 16:52:38 -0400

    updating deleted token bucket algorithm

--- token_bucket.lua
+++ token_bucket.lua
@@ -1,111 +1,53 @@
-#include <sw/redis++/redis++.h>
+local key = KEYS[1]
 
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <thread>
-#include <chrono>
+local capacity = tonumber(ARGV[1])
+local refill_rate = tonumber(ARGV[2])
+local now = tonumber(ARGV[3])
+local requested = tonumber(ARGV[4])
 
-using namespace sw::redis;
-using namespace std;
+--current bucket state
+local data = redis.call(
+    "HMGET",
+    key,
+    "tokens",
+    "last_refill"
+)
 
-string loadScript(const std::string& filename) {
-    ifstream file(filename);
+local tokens = tonumber(data[1])
+local last_refill = tonumber(data[2])
 
-    if (!file.is_open()) {
-        throw runtime_error("Could not open Lua script");
-    }
+if tokens == nil then
+    tokens = capacity
+    last_refill = now
+end
 
-    return string(
-        (istreambuf_iterator<char>(file)),
-        istreambuf_iterator<char>()
-    );
+--time passed
+local elapsed = now - last_refill
+
+tokens = tokens + (elapsed * refill_rate)
+
+if tokens > capacity then
+    tokens = capacity
+end
+
+local allowed = 0
+
+if tokens >= requested then
+    tokens = tokens - requested
+    allowed = 1
+end
+
+-- save updated bucket state
+redis.call(
+    "HSET",
+    key,
+    "tokens",
+    tokens,
+    "last_refill",
+    now
+)
+
+return {
+    allowed,
+    tokens
 }
-
-int main() {
-    try {
-        Redis redis("tcp://127.0.0.1:6379");
-        cout << "Connected to Redis!\n";
-
-        string script = loadScript("token_bucket.lua");
-
-        auto script_sha = redis.script_load(script);
-
-        cout << "Loaded Lua script:\n";
-        cout << script_sha << "\n\n";
-
-        string key = "ratelimit:user123";
-
-        int capacity = 10;
-        double refill_rate = 1;  
-        int requested = 1;
-
-        for (int i = 1; i <= 15; i++) {
-
-            auto now = chrono::duration<double>(
-                chrono::system_clock::now().time_since_epoch()
-            ).count();
-
-            vector<string> keys = {
-                key
-            };
-
-            vector<string> args = {
-                to_string(capacity),
-                to_string(refill_rate),
-                to_string(now),
-                to_string(requested)
-            };
-
-            auto result = redis.evalsha<vector<long long>>(
-                script_sha,
-                keys.begin(),
-                keys.end(),
-                args.begin(),
-                args.end()
-            );
-
-            int allowed = static_cast<int>(result[0]);
-            double remaining = result[1];
-
-            if (allowed) {
-                cout
-                    << "Request "
-                    << i
-                    << ": ALLOWED | Tokens left: "
-                    << remaining
-                    << "\n";
-            }
-            else {
-                cout
-                    << "Request "
-                    << i
-                    << ": BLOCKED | Tokens left: "
-                    << remaining
-                    << "\n";
-            }
-
-            this_thread::sleep_for(
-                chrono::milliseconds(100)
-            );
-        }
-
-    } catch (const Error &err) {
-
-        cerr
-            << "Redis error: "
-            << err.what()
-            << "\n";
-
-    } catch (const exception &err) {
-
-        cerr
-            << "Error: "
-            << err.what()
-            << "\n";
-    }
-
-    return 0;
-}
-
