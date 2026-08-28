b71e187a6f70e82f172f48a5b68485974620e638
ninashapranjal <shahipranjal7@gmail.com>
2026-08-28 18:54:18 -0400

    build + fixing token bucket for return type errors

---
+++ build/CMakeFiles/rate_limiter.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pshahi/CodingProjects/distributed-rate-limiter/main.cpp" "CMakeFiles/rate_limiter.dir/main.cpp.o" "gcc" "CMakeFiles/rate_limiter.dir/main.cpp.o.d"
+  "/home/pshahi/CodingProjects/distributed-rate-limiter/src/config.cpp" "CMakeFiles/rate_limiter.dir/src/config.cpp.o" "gcc" "CMakeFiles/rate_limiter.dir/src/config.cpp.o.d"
+  "/home/pshahi/CodingProjects/distributed-rate-limiter/src/fixed_window.cpp" "CMakeFiles/rate_limiter.dir/src/fixed_window.cpp.o" "gcc" "CMakeFiles/rate_limiter.dir/src/fixed_window.cpp.o.d"
+  "/home/pshahi/CodingProjects/distributed-rate-limiter/src/sliding_window_counter.cpp" "CMakeFiles/rate_limiter.dir/src/sliding_window_counter.cpp.o" "gcc" "CMakeFiles/rate_limiter.dir/src/sliding_window_counter.cpp.o.d"
+  "/home/pshahi/CodingProjects/distributed-rate-limiter/src/token_bucket.cpp" "CMakeFiles/rate_limiter.dir/src/token_bucket.cpp.o" "gcc" "CMakeFiles/rate_limiter.dir/src/token_bucket.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/rate_limiter.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rate_limiter.dir/main.cpp.o"
+  "CMakeFiles/rate_limiter.dir/main.cpp.o.d"
+  "CMakeFiles/rate_limiter.dir/src/config.cpp.o"
+  "CMakeFiles/rate_limiter.dir/src/config.cpp.o.d"
+  "CMakeFiles/rate_limiter.dir/src/fixed_window.cpp.o"
+  "CMakeFiles/rate_limiter.dir/src/fixed_window.cpp.o.d"
+  "CMakeFiles/rate_limiter.dir/src/sliding_window_counter.cpp.o"
+  "CMakeFiles/rate_limiter.dir/src/sliding_window_counter.cpp.o.d"
+  "CMakeFiles/rate_limiter.dir/src/token_bucket.cpp.o"
+  "CMakeFiles/rate_limiter.dir/src/token_bucket.cpp.o.d"
+  "rate_limiter"
+  "rate_limiter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rate_limiter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
