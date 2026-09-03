d9421ae041d8c6c5dc5f06b4b49514de6c3d4782
ninashapranjal <shahipranjal7@gmail.com>
2026-09-03 19:11:49 -0400

    var sliding_window_counter.lua

--- lua/sliding_window_counter.lua
+++ lua/sliding_window_counter.lua
@@ -1,20 +1,15 @@
-local current_count =
-    tonumber(redis.call("GET", KEYS[1])) or 0
-
-local previous_count =
-    tonumber(redis.call("GET", KEYS[2])) or 0
-
-local elapsed =
-    tonumber(ARGV[1])
-
-local window =
-    tonumber(ARGV[2])
-
-local requested =
-    tonumber(ARGV[3])
-
-local limit =
-    tonumber(ARGV[4])
+local base_key = KEYS[1]
+local window = tonumber(ARGV[1])
+local requested = tonumber(ARGV[2])
+local limit = tonumber(ARGV[3])
+local time = redis.call("TIME")
+local now = tonumber(time[1]) + tonumber(time[2]) / 1000000
+local current_window = math.floor(now / window)
+local elapsed = now - current_window * window
+local current_key = base_key .. ":" .. current_window
+local previous_key = base_key .. ":" .. (current_window - 1)
+local current_count = tonumber(redis.call("GET", current_key)) or 0
+local previous_count = tonumber(redis.call("GET", previous_key)) or 0
 
 -- how much of the prev window
 -- should contribute to the curr window
@@ -32,16 +27,14 @@ if estimated_count + requested <= limit then
 
     redis.call(
         "INCRBY",
-        KEYS[1],
+        current_key,
         requested
     )
 
-    -- keep curr window key alive for one complete window
-    redis.call(
-        "EXPIRE",
-        KEYS[1],
-        window
-    )
+    -- It is needed throughout the next window as the previous bucket.
+    if current_count == 0 then
+        redis.call("EXPIRE", current_key, window * 2)
+    end
 
     return 1
 end
